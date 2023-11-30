67126a98f85eb8f746eca52486a219a237334b32
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-11-30 17:49:23 +0100

    correction -> va check discord le san

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: install run
+
+run:
+	poetry run python kdna/main.py
--- kdna/main.py
+++ kdna/main.py
@@ -1,10 +1,30 @@
-"""Main entry point for kdna"""""
+from Backup import Backup
+from Server import Server
+from ConfUtils import ConfUtils
 
 
 def main():
-    """Main entry point for kdna"""""
-    print("Hello DO")
+    ConfUtils.initialize_config_file()
+
+    backup = Backup("9", "monthly", "okay", "2021-01-01", "3", "/home/backup")
+    backup.add()
+
+    server = Server("18", "credentials", "22", "hello")
+    server.add()
+
+    Backup.delete("9")
+    Backup.delete("2")
+
+    Server.delete("4")
+    Server.delete("alisas", by_alias=True)
+
+    Server.update("test", new_port="25",
+                  new_credentials="test", new_alias="ahahah")
+    Backup.update("5", new_frequency="daily",
+                  new_timestamp="2021-01-02", new_path="/home/backup")
+
+    ConfUtils.readAll()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
