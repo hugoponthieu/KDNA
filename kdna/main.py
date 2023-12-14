b93fec4c763daec00b3213bbeb80d00fed70144d
je-suis-professionnelle <151126350+je-suis-professionnelle@users.noreply.github.com>
2023-12-14 19:44:20 +0100

    Update main.py

--- kdna/main.py
+++ kdna/main.py
@@ -26,7 +26,7 @@ def backup():
 
 @kdna.group()
 def server():
-    """Commande pour lancer le serveur"""
+    """Commande pour gérer le serveur"""
 
 
 @kdna.group()
@@ -35,11 +35,11 @@ def autobackup():
 
 
 @backup.command()
-@click.option('-t', '--namepath', nargs=2, help="entrer le nom "
+@click.option('-t', '--namepath', nargs=2, required=True, help="entrer le nom "
                                                 "et le chemin du fichier à sauvegarder")
 def add(namepath):
-    """add: Commande pour simuler l'ajout d'un fichier à la sauvegarde
-        -t, --namepath: option pour entrer le nom et le chemin du fichier à sauvegarder"""
+    """Commande pour sauvegarder\n
+        -t : --namepath: option pour entrer le nom et le chemin du fichier à sauvegarder"""
     name, path = namepath
     click.echo(f"Created backup \"{name}\":v1.1.2")
     click.echo("Contains :")
@@ -49,34 +49,37 @@ def add(namepath):
 @server.command()
 @click.argument('name')
 def set(name):
-    """set: Commande pour sélectionner un serveur
+    """Commande pour sélectionner un serveur\n
         name: argument pour entrer le nom du serveur"""
     click.echo(f"Picked server : \"{name}\"")
 
 
 @autobackup.command()
-@click.option('-n', '--nameofcron', nargs=1, help="entrer le nom du cron")
-@click.option('-t', '--tag', nargs=1, help="entrer le tag")
+@click.option('-n', '--nameofcron', nargs=1, required=True, help="entrer le nom du cron")
+@click.option('-t', '--tag', nargs=1, required=True, help="entrer le tag")
 @click.argument('cron_schedule', type=click.Choice(['daily', 'monthly', 'weekly',
                                                     'custom']), required=True)
 @click.argument('custom_cron', nargs=-1)
 def schedule(nameofcron, tag, cron_schedule, custom_cron):
-    """schedule: Commande pour prévoir une backup régulière
-        --nameofcron: option pour entrer le nom du cron
-        --tag: option pour saisir un tag pour le cron
-        cron_schedule: argument pour choisir le schedule du cron
-        custom_cron: argument pour entrer un cron personnalisé"""
+    """Commande pour prévoir une backup régulière\n
+        -n : --nameofcron: option pour entrer le nom du cron\n
+        -t : --tag: option pour saisir un tag pour le cron\n
+        --cron_schedule: argument pour choisir le schedule du cron\n
+        --custom_cron: argument pour entrer un cron personnalisé"""
     click.echo(f"Name of cron : \"{nameofcron}\"")
     click.echo(f"Cron tag and schedule : \"{tag}\" \"{cron_schedule}\"")
     if cron_schedule == 'custom':
-        click.echo(f"Custom cron : \"{custom_cron}\"")
+        if not custom_cron:
+            click.echo("L'argument custom_cron doit être suivi d'un schedule de cron personnalisé.")
+        else:
+            click.echo(f"Custom cron : \"{custom_cron}\"")
 
 
 @autobackup.command()
-@click.option('-n', '--nameofcron', nargs=1, help="entrer le nom du cron à stopper")
+@click.option('-n', '--nameofcron', nargs=1, required=True, help="entrer le nom du cron à stopper")
 def stop(nameofcron):
-    """schedule: Commande pour stopper une backup régulière
-        --nameofcron: option pour entrer le nom du cron à stopper"""
+    """Commande pour stopper une backup régulière\n
+        -n : --nameofcron: option pour entrer le nom du cron à stopper"""
     click.echo(f"Stopped cron : \"{nameofcron}\"")
 
 
